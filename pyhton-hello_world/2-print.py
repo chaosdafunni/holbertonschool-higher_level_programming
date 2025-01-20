d4842de4269f38015a3bf8616740eba281b6cfc5
william <joshthefunnyalr@gmail.com>
2025-01-19 21:37:00 -0400

    printing exactly what task 0 wanted

---
+++ pyhton-hello_world/2-print.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This script prints a motivational programming quote.
+The output is formatted as: "Programming is like building a zzle"
+"""
+
+# Print the desired message to the console
+print(
+        "\"Programming is like building a multilingual puzzle"
+        )
