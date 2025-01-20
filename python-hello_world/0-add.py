f08add07b3e610358275a1cf4e2198d50bb4d80f
william <joshthefunnyalr@gmail.com>
2025-01-19 22:04:22 -0400

    had to move everything here because i did it in the wrong dir

---
+++ python-hello_world/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1  # Assign the first value
+    b = 2  # Assign the second value
+    result = add(a, b)  # Call the add function
+
+    # Print the result in the required format
+    print(f"{a} + {b} = {result}")
---
+++ python-if_else_loops_functions/3-print_number.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This script prints a formatted message using an f-string.
+The message displays a number followed by the text "Battery street".
+"""
+
+# Define the number variable
+number = 98
+
+# Print the number followed by "Battery street" using an f-string
+print(f"{number} Battery street")
